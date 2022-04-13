85d228fb04d6594140b5c71ba58f0c9a93cfbfd7
nealichinohe <neal.kuni@yahoo.com>
2022-04-13 11:07:53 -0600

    added progress bar function.

--- script.js
+++ script.js
@@ -26,3 +26,65 @@ function loadSong(song) {
   audio.src = `music/${song}.mp3`;
   cover.src = `images/${song}.jpeg`;
 }
+
+function playSong(){
+  musicContainer.classList.add('play')
+  playBtn.querySelector('i.fas').classList.remove('fa-play')
+  playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+  audio.play();
+}
+function pauseSong(){
+  musicContainer.classList.remove('play')
+  playBtn.querySelector('i.fas').classList.add('fa-play')
+  playBtn.querySelector('i.fas').classList.remove('fa-pause')
+
+  audio.pause()
+}
+
+function prevSong() {
+  songIndex--
+  if (songIndex < 0){
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex])
+  playSong()
+}
+
+function nextSong(){
+  songIndex++
+
+  if (songIndex > songs.length - 1){
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex])
+  playSong()
+}
+
+function updateProgess(e){
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration ) * 100;
+  progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(){
+  
+}
+
+// Event listeners
+playBtn.addEventListener('click', ()=> {
+  const isPlaying = musicContainer.classList.contains("play")
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+})
+
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgess)
+progressContainer.addEventListener('click', setProgress)
